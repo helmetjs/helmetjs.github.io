50556e2e6e6259a946bfd27cdbc78c4914ad6a65
Oashe02 <mehtaoashe2006@gmail.com>
2025-11-17 11:55:15 +0530

    added css in global file

--- static/js/copy-code.js
+++ static/js/copy-code.js
@@ -7,11 +7,14 @@
     blocks.forEach((code) => {
       const pre = code.parentElement;
       pre.style.position = "relative";
+
       const btn = document.createElement("button");
       btn.className = "copy-code-button";
       btn.innerHTML =
         '<span class="material-symbols-outlined">content_copy</span>';
+
       pre.appendChild(btn);
+
       btn.addEventListener("click", async () => {
         try {
           await navigator.clipboard.writeText(code.innerText.trim());
@@ -33,4 +36,3 @@
     });
   });
 })();
-
